feb12608560c4e5787b4a2aa410f8ab3323c6285
Muhammad Shariq Shafiq <107953192+M-Shariq-546@users.noreply.github.com>
2022-08-26 05:12:03 +0500

    Updated code for 100 values

--- Graph 4.py
+++ Graph 4.py
@@ -1,17 +1,18 @@
 from matplotlib import pyplot as plt
-import numpy 
-x = [1, 2, 3, 4 , 5 , 6 , 7, 8, 9, 10]
-
-
+import numpy
+x = []
 y = []
 
-for i in range(1 , 11):
-    z =numpy.log(i)
+for i in range(1 , 100):
+    x.insert(i , i)
+
+for i in range(1 , 100):
+    z = numpy.log(i)
     y.insert(i , z)
     
 print("Y is = ",y)
 print("X is = ",x)
 
-plt.title("Graph of LogX")
-plt.plot([1, 2, 3, 4 , 5 , 6 , 7, 8, 9, 10] , [0.0, 0.6931471805599453, 1.0986122886681098, 1.3862943611198906, 1.6094379124341003, 1.791759469228055, 1.9459101490553132, 2.0794415416798357, 2.1972245773362196, 2.302585092994046])
+plt.title("Graph of logX")
+plt.plot(x , y)
 plt.show()
