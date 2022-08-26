0973783f9e94d04142229766de583e8c09434cea
Muhammad Shariq Shafiq <107953192+M-Shariq-546@users.noreply.github.com>
2022-08-26 05:09:17 +0500

    Updated code for 100 values

--- Graph 1.py
+++ Graph 1.py
@@ -1,15 +1,18 @@
 from matplotlib import pyplot as plt
 
-x = [1, 2, 3, 4 , 5 , 6 , 7, 8, 9, 10]
-
+x = []
 y = []
-for i in range(1 , 11):
-    z = (2 * i)
-    y.insert(i , z)
 
+for i in range(1 , 100):
+    x.insert(i , i)
+
+for i in range(1 , 100):
+    z = 2 * i
+    y.insert(i , z)
+    
 print("Y is = ",y)
 print("X is = ",x)
- 
+
 plt.title("Graph of 2X")
-plt.plot([1,2,3,4,5,6,7,8,9,10] , [2,4 , 6, 8,10 , 12 , 14 , 16 , 18 , 20])
+plt.plot(x , y)
 plt.show()
