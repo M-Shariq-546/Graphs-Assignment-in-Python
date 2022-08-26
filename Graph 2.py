8c39501868160f4e13eccaea3a19fd587a30076a
Muhammad Shariq Shafiq <107953192+M-Shariq-546@users.noreply.github.com>
2022-08-26 05:10:10 +0500

    Updated code for 100 values

--- Graph 2.py
+++ Graph 2.py
@@ -1,18 +1,18 @@
 from matplotlib import pyplot as plt
 
-x = [1, 2, 3, 4 , 5 , 6 , 7, 8, 9, 10]
-
-
+x = []
 y = []
 
-for i in range(1 , 11):
-    z = (3 * i)
-    y.insert(i , z)
+for i in range(1 , 100):
+    x.insert(i , i)
 
+for i in range(1 , 100):
+    z = 3 * i
+    y.insert(i , z)
+    
 print("Y is = ",y)
 print("X is = ",x)
 
 plt.title("Graph of 3X")
-
-plt.plot([1 , 2 ,3, 4, 5, 6 , 7, 8, 9, 10] , [3, 6, 9, 12, 15, 18, 21, 24, 27 , 30])
+plt.plot(x , y)
 plt.show()
